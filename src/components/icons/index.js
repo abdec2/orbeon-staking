1e4fe26718bbbd8f9f325ff3aa8ca62653900a43
abdec2 <abdec2@hotmail.com>
2023-03-08 07:23:41 +0300

    working on design

---
+++ src/components/icons/index.js
@@ -0,0 +1,19 @@
+export const HomeIcon = () => {
+    return (
+        <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 5H0V15H6V5Z" fill="#F5331E" />
+            <path d="M14 5H8V15H14V5Z" fill="#F5331E" />
+            <path d="M6 0H0V3H6V0Z" fill="#F5331E" />
+            <path d="M14 0H8V3H14V0Z" fill="#F5331E" />
+        </svg>
+    );
+};
+
+export const ChevronDown = () => {
+    return (
+        <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.34427 -5.94011e-05L0.930054 1.41415L8.00112 8.48522L9.41533 7.07101L2.34427 -5.94011e-05Z" fill="#000515" />
+            <path d="M15.0742 1.41421L13.66 0L6.58897 7.07107L8.00318 8.48528L15.0742 1.41421Z" fill="#000515" />
+        </svg>
+    )
+}
---
+++ src/layout/MainLayout/Header/HeaderContent/index.js
@@ -0,0 +1,77 @@
+// material-ui
+import { Box, IconButton, Link, useMediaQuery, Typography, Button } from '@mui/material';
+import { GithubOutlined } from '@ant-design/icons';
+
+// project import
+import Search from './Search';
+import Profile from './Profile';
+import Notification from './Notification';
+import MobileSection from './MobileSection';
+
+
+// ==============================|| HEADER - CONTENT ||============================== //
+
+const HeaderContent = () => {
+    const matchesXs = useMediaQuery((theme) => theme.breakpoints.down('md'));
+
+    return (
+        <>
+            <Box sx={{ 
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    width: '100%'
+                    
+                }}
+            >
+                <Typography variant="h4" sx={{
+                    ml: 2
+                }}>
+                    Staking
+                </Typography>
+                <Box sx={{display: 'flex', alignItems: 'center'}}>
+                    <div style={{width: '1px', height:'28px', background:'#C7C8CC', marginRight: '9px'}}></div>
+                    <Button sx={{
+                        padding: '8px 22px', 
+                        fontFamily: 'Space Grotesk',
+                        fontSize: '14px',
+                        boxSizing: 'border-box',
+                        background: 'none',
+                        color: '#000515',
+                        fontWeight: 400,
+                        border: '1px solid #000515',
+                        borderRadius: 0, 
+                        '&:hover' : {
+                            bgcolor: '#C7C8CC80'
+                        }
+                    }}>Connect Wallet</Button>
+                    <div style={{width: '1px', height:'28px', background:'#C7C8CC', marginLeft: '9px', marginRight: '20px'}}></div>
+                    {!matchesXs && <Profile />}
+                    {matchesXs && <MobileSection />}
+                </Box>
+            </Box>
+            
+            {/* {!matchesXs && <Search />}
+            {matchesXs && <Box sx={{ width: '100%', ml: 1 }} />}
+            
+
+            <IconButton
+                component={Link}
+                href="https://github.com/codedthemes/mantis-free-react-admin-template"
+                target="_blank"
+                disableRipple
+                color="secondary"
+                title="Download Free Version"
+                sx={{ color: 'text.primary', bgcolor: 'grey.100' }}
+            >
+                <GithubOutlined />
+            </IconButton>
+
+            <Notification />
+            {!matchesXs && <Profile />}
+            {matchesXs && <MobileSection />} */}
+        </>
+    );
+};
+
+export default HeaderContent;
